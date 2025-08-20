6138de191930da887a32d6fbf2e56643b140a2d7
Kuba / WCHC <84104920+Wiechciu@users.noreply.github.com>
2025-08-20 21:14:15 +0200

    Fixed typo

--- addons/mixamo_animation_retargeter/mixamo_animation_retargeter.gd
+++ addons/mixamo_animation_retargeter/mixamo_animation_retargeter.gd
@@ -8,9 +8,9 @@ var context_menu_plugin_instance: EditorContextMenuPlugin
 func _enter_tree() -> void:
 	context_menu_plugin_instance = preload("res://addons/mixamo_animation_retargeter/context_menu.gd").new()
 	add_context_menu_plugin(EditorContextMenuPlugin.CONTEXT_SLOT_FILESYSTEM, context_menu_plugin_instance)
-	print("Mixamo Animatino Retargeter loaded.")
+	print("Mixamo Animation Retargeter loaded.")
 
 
 func _exit_tree() -> void:
 	remove_context_menu_plugin(context_menu_plugin_instance)
-	print("Mixamo Animatino Retargeter unloaded.")
+	print("Mixamo Animation Retargeter unloaded.")
